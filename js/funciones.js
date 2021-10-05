bff36a1b3fc1cbc095b4667bcfa3cc5434ce596e
martamorall <martamoral22@gmail.com>
2021-10-05 16:34:04 +0200

    first commit

---
+++ js/funciones.js
@@ -0,0 +1,51 @@
+var palabra=document.getElementById("palabra");
+var letra=document.getElementById("letra");
+var resultado=document.getElementById("resultado");
+var cajaFallos=document.getElementById("cajaFallos");
+var palabraA;
+var guiones;
+
+//Evento en el que al perder el foco del input palabra, se forman los guiones
+document.addEventListener("blur", crearGuiones, true);
+document.addEventListener("change", buscarCaracter, false)
+
+//Reemplaza cada caracter de la palabra insertada por guiones y los muestra en el espacio de resultado
+function crearGuiones(){
+    palabraA=palabra.value;
+    guiones=palabraA.replace(/[a-z]/gi, "-");
+    resultado.value=guiones;
+    //alert(resultado.value);
+}
+
+//Busca el caracter insertado en la palabra y si lo encuentra, sustituye el guion por dicho caracter.
+function buscarCaracter(){
+    if (letra.value==""){
+        return;
+    };
+    
+    palabraA=palabra.value.toUpperCase();
+    var caracterBuscar=(letra.value).toUpperCase();
+    var posicion=palabraA.indexOf(caracterBuscar);
+    var es_acierto=false;
+
+    while(posicion>-1){
+        //Cambia el guion por el caracter introducido
+        guiones=guiones.substring(0, posicion)+caracterBuscar+guiones.substr(posicion+1, guiones.length);
+        
+        //Copia en resultado los guiones con la letra acertada
+        resultado.value=guiones;
+
+        //Para seguir buscando, la posición tiene que aumentar (+1)
+        posicion=palabraA.indexOf(caracterBuscar, posicion+1);
+        es_acierto=true;
+    }
+
+    //En caso de que el caracter insertado no coincida con ninguno de la palabra, lo muestra en párrafos  
+    if (!palabraA.includes(caracterBuscar)){
+        var fallos=document.createElement("p");
+        var letraFallada=document.createTextNode(caracterBuscar);
+        fallos.appendChild(letraFallada);
+        cajaFallos.appendChild(fallos);
+        }
+}
+    
